3e3534c05da092ddb6ea14acdc9a8d6542e3d665
Julie Langmade <jlangmade@gmail.com>
2015-09-04 08:53:21 -0500

    added arrive view

--- src/js/components/get-started.js
+++ src/js/components/get-started.js
@@ -26,6 +26,12 @@ class GetStarted extends React.Component {
         </header>
         <section>
           <h2>{message}</h2>
+          <p>You're about to go on a fun adventure through Downtown Nashville. There are 10 stops on the adventure.  Here's what you need to know.</p>
+          <ul>
+            <li>Walking directions from one stop to the next are provided</li>
+            <li>Enter an unlock code to reveal the next location</li>
+            <li>Have fun and enjoy Nashville!</li>
+          </ul>
           <button onClick={this.onSubmit.bind(this)} ref="started">Get Started</button>
         </section>
       </div>
