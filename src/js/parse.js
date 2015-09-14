6af00f6d79f16d345c7b2cff152fc3c7bd989154
Julie Langmade <jlangmade@gmail.com>
2015-09-13 23:26:58 -0500

    Update 2015-09-14T04:26:53.841Z

--- src/js/parse.js
+++
@@ -1,6 +0,0 @@
-let {Parse} = require('parse');
-
-Parse.initialize("ChLISqsdkrBjZhWfxblTnPuAsYj9ktNDV5f7VX10",
-                "wUrL8DMSMynm7TXVQoUJ7g27jZwS7mCmvaTLiBPT");
-
-export default Parse;
