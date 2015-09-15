11cc8034f3b1c0609bb42ff5670cc033ced78685
Julie Langmade <jlangmade@gmail.com>
2015-09-14 22:39:09 -0500

    added new image slider

--- src/js/components/image-slider/image-slider.js
+++ src/js/components/image-slider/image-slider.js
@@ -1,26 +1,25 @@
 import React, {PropTypes} from 'react';
-import $ from 'jquery';
+import Slider from 'react-slick';
 
 class ImageSlider extends React.Component {
-  componentDidMount() {
-      $(function() {
-        $('#slides').slidesjs({
-          width: 940,
-          height: 528
-        });
-      });
-  }
-
   render() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
     return (
-      <div id="slides">
-        <img src="http://placehold.it/940x528"/>
-        <img src="http://placehold.it/940x528"/>
-        <img src="http://placehold.it/940x528"/>
-        <img src="http://placehold.it/940x528"/>
-        <img src="http://placehold.it/940x528"/>
-      </div>
-    )
+      <Slider {...settings}>
+        <div><img src="img/records.jpg"/></div>
+        <div><img src="img/bootsguitar.jpg"/></div>
+        <div><img src="img/musicvenue.jpg"/></div>
+        <div><h3>4</h3></div>
+        <div><h3>5</h3></div>
+        <div><h3>6</h3></div>
+      </Slider>
+    );
   }
 };
 
